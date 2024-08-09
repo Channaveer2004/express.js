10fd568dfc575842c805a8ba6482248c427da1bb
Channaveer <channaveerbs0905@gmail.com>
2024-08-09 15:53:13 +0530

    mongo

--- Zod/Zod/zod1.js
+++ Zod/Zod/zod1.js
@@ -7,22 +7,22 @@ const zod = require('zod');
 
 // If this is array of numbers with atleast 1 input, return true else return false
 
-function validateInput(arr){
+function validateInput(arr) {
     const schema = zod.array(zod.number());
 
     const response = schema.safeParse(arr);
     console.log(response);
 }
 
-validateInput([1,2,3,4]); // Success
-validateInput([1,2,'3',4]); // Failure
+validateInput([1, 2, 3, 4]); // Success
+validateInput([1, 2, '3', 4]); // Failure
 
 
 
 
 // The use case could be to validate a objects with specific keys and values, then we can use object() method of zod.
 
-function validateObject(obj){
+function validateObject(obj) {
     const schema = zod.object({
         email: zod.string().email(),
         password: zod.string().min(6),
